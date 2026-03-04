c2ad539bd6ed6669c085fa65535c57850393cebc
LameeAlharkosh <jokerasamt2020@gmail.com>
2026-03-04 21:22:49 +0300

    Proje dosyası ekle.

---
+++ FlappyBird_V2/Form1.cs
@@ -0,0 +1,109 @@
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace FlappyBird_V2
+{
+    public partial class Form1 : Form
+    {
+        // إعدادات اللعبة المتوازنة
+        int pipeSpeed = 8;
+        int gravity = 5;
+        int score = 0;
+        bool isGameOver = false;
+        Random rnd = new Random();
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.KeyPreview = true; // تفعيل الكيبورد لزر R
+
+            // حل مشكلة المربعات البيضاء آلياً
+            bird.BackColor = Color.Transparent;
+            pipeTop.BackColor = Color.Transparent;
+            pipeBottom.BackColor = Color.Transparent;
+            scoreText.BackColor = Color.Transparent;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            bird.Top += gravity;
+            pipeBottom.Left -= pipeSpeed;
+            pipeTop.Left -= pipeSpeed;
+            scoreText.Text = "Score: " + score;
+
+            // حل مشكلة التأخر: التوليد يبدأ بمجرد خروج الأنبوب بمسافة 75 بكسل
+            if (pipeBottom.Left < -75)
+            {
+                score++;
+                GeneratePipes();
+            }
+
+            // فحص الاصطدام
+            if (bird.Bounds.IntersectsWith(pipeBottom.Bounds) ||
+                bird.Bounds.IntersectsWith(pipeTop.Bounds) ||
+                bird.Bounds.IntersectsWith(ground.Bounds) || bird.Top < -25)
+            {
+                EndGame();
+            }
+        }
+
+        private void GeneratePipes()
+        {
+            int randomY = rnd.Next(-150, 50);
+            // الإبعاد لـ 950 لكي يتغير الارتفاع "خلف الكواليس" ولا يراه اللاعب
+            pipeTop.Left = 950;
+            pipeTop.Top = randomY;
+
+            pipeBottom.Left = 950;
+            pipeBottom.Top = randomY + 380;
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (!isGameOver) gravity = -14; // قفزة هادئة (ليست سريعة جداً)
+        }
+
+        private void Form1_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (!isGameOver) gravity = 5;
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.R && isGameOver)
+            {
+                RestartGame();
+            }
+        }
+
+        private void EndGame()
+        {
+            isGameOver = true;
+            timer1.Stop();
+            scoreText.Text = "Score: " + score + " | اضغط R للإعادة";
+        }
+
+        private void RestartGame()
+        {
+            isGameOver = false;
+            score = 0;
+            gravity = 5;
+            bird.Top = 150;
+
+            // العودة لوضعية بداية مرتبة
+            pipeTop.Top = -100;
+            pipeBottom.Top = 280;
+            pipeTop.Left = 850;
+            pipeBottom.Left = 850;
+
+            scoreText.Text = "Score: 0";
+            timer1.Start();
+        }
+
+        // دوال فارغة لمنع أخطاء التصميم
+        private void pipeTop_Click(object sender, EventArgs e) { }
+        private void Form1_Load(object sender, EventArgs e) { }
+        private void Form1_Load_1(object sender, EventArgs e) { }
+    }
+}
